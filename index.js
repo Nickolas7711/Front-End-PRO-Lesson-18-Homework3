0be782e9c4bf5da534173080f241f4a96875e3ca
Mykola Kuniev <Nickolas7711@gmail.com>
2023-07-17 10:18:10 +0300

    Lesson 18/ homework 3

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ let CATEGORIES = {
         name: "Ноутбуки",
         items: [
             { 
-                img: '../Styles/images/products/275056538.webp',
+                img: './Styles/images/products/275056538.webp',
                 name: 'Ноутбук Acer Aspire 7 A715-42G-R3EZ', 
                 description: `Экран 15.6" IPS (1920x1080) Full HD, матовый / AMD Ryzen 5 5500U (2.1 - 4.0 ГГц) / RAM 16 ГБ / SSD 512 ГБ / nVidia GeForce GTX 1650, 4 ГБ / без ОД / LAN / Wi-Fi / Bluetooth / веб-камера / без ОС / 2.15 кг / черный`,
                 price: 28999,
             },
 
             { 
-                img: '../Styles/images/products/290842123.webp',
+                img: './Styles/images/products/290842123.webp',
                 name: 'Ноутбук ASUS Laptop X515EA-BQ2066',
                 description: `Экран 15.6" IPS (1920x1080) Full HD, матовый / Intel Core i3-1115G4 (3.0 - 4.1 ГГц) / RAM 12 ГБ / SSD 512 ГБ / Intel UHD Graphics / без ОД / Wi-Fi / Bluetooth / веб-камера / без ОС / 1.8 кг / серый`,
                 price: 16999, 
             },
 
             { 
-                img: '../Styles/images/products/320929254.webp',
+                img: './Styles/images/products/320929254.webp',
                 name: 'Ноутбук Lenovo IdeaPad 3 15IAU7',
                 description: `Екран 15.6" IPS (1920x1080) Full HD, матовий / Intel Core i5-1235U (3.3 - 4.4 ГГц) / RAM 16 ГБ / SSD 512 ГБ / Intel Iris Xe Graphics / без ОД / Wi-Fi / Bluetooth / веб-камера / без ОС / 1.63 кг / сірий`,
                 price: 23999,
@@ -28,21 +28,21 @@ let CATEGORIES = {
         name: "Комп'ютери",
         items: [
             { 
-                img: '../Styles/images/products/277930782.webp',
+                img: './Styles/images/products/277930782.webp',
                 name: 'Комп\'ютер Artline Gaming', 
                 description: `AMD Ryzen 5 3600 (3.6 - 4.2 ГГц) / RAM 16 ГБ / HDD 1 ТБ + SSD 480 ГБ / nVidia GeForce RTX 3050, 8 ГБ / без ОД / LAN / без ОС`,
                 price: 26599,
             },
 
             { 
-                img: '../Styles/images/products/311809510.webp',
+                img: './Styles/images/products/311809510.webp',
                 name: 'Комп\'ютер ARTLINE Gaming X63',
                 description: `AMD Ryzen 5 5600 (3.5 - 4.4 ГГц) / RAM 16 ГБ / SSD 1 ТБ / nVidia GeForce RTX 3060 Ti, 8 ГБ / без ОД / LAN / без ОС`,
                 price: 34999, 
             },
 
             { 
-                img: '../Styles/images/products/318910162.webp',
+                img: './Styles/images/products/318910162.webp',
                 name: 'Моноблок Lenovo IdeaCentre 3 24IAP7',
                 description: `Екран 23.8" WVA (1920x1080) Full HD / Intel Core i3-1215U (4.4 ГГц) / RAM 8 ГБ / SSD 256 ГБ / Intel UHD Graphics / без ОД / LAN / Wi-Fi / Bluetooth / веб-камера / без ОС / 7.03 кг / чорний / клавіатура + миша`,
                 price: 21999,
@@ -53,21 +53,21 @@ let CATEGORIES = {
         name: "Планшети",
         items: [
             { 
-                img: '../Styles/images/products/224010065.webp',
+                img: './Styles/images/products/224010065.webp',
                 name: 'Планшет Apple iPad 10.2" 2021 Wi-Fi 64 GB Silver', 
                 description: `Екран 10.2" IPS (2160x1620) MultiTouch / Apple A13 Bionic (2.65 ГГц) / 64 ГБ вбудованої пам'яті / Wi-Fi / Bluetooth 4.2 / основна камера 8 Мп, фронтальна — 12 Мп / iPadOS 15 / 487 г / сріблястий`,
                 price: 16599,
             },
 
             { 
-                img: '../Styles/images/products/186993631.webp',
+                img: './Styles/images/products/186993631.webp',
                 name: 'Планшет Samsung Galaxy Tab A7 Lite LTE 32GB Grey',
                 description: `Екран 8.7" (1340x800) MultiTouch/MediaTek Helio P22T (2.3 ГГц)/RAM 3 ГБ/32 ГБ вбудованої пам'яті + microSD/3G/4G/Wi-Fi/Bluetooth 5.0/основна камера 8 Мп, фронтальна — 2 Мп/GPS/ГЛОНАСС/Android /371 г/сірий`,
                 price: 6199, 
             },
 
             { 
-                img: '../Styles/images/products/259395026.webp',
+                img: './Styles/images/products/259395026.webp',
                 name: 'Планшет Apple iPad Air 10.9" M1 Wi-Fi 64GB Space Gray',
                 description: `Екран 10.9" (2360x1640) MultiTouch / Apple M1 / RAM 8 ГБ / 64 ГБ вбудованої пам'яті / Wi-Fi / Bluetooth 5.0 / основна камера 12 Мп, фронтальна 12 Мп / iPadOS 15 / 461 г / космічний сірий`,
                 price: 29999,
@@ -224,15 +224,3 @@ function displayError(errorMessage, container) {
     }, 2500);
 
 }
-
-// Делаем на основании прошедшего дз. 
-// В информации товара – кнопка "купить". 
-// При нажатии на "Купить" ниже появляется форма оформления заказа со следующими полями: 
-// ФИО покупателя 
-// Город (выбор из списка) 
-// Состав Новой почты для отправки 
-// Наложенные платежи или оплаты банковской карты 
-// Количество покупаемой продукции 
-// Комментарий к заказу 
-// 2. Реализовать проверку всех пользовательских данных при подтверждении заказа - обязательные поля заполнены. Иначе – выводить ошибку на страницу 
-// 3. Выводить информацию о заказе на страницу (информация о товаре и о доставке)
